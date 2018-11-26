16bc71e9d2f2f67459eaa18ac8b76daa2c9ae0d5
Aditya Singh <outforaditya@gmail.com>
2018-11-26 05:42:07 +0530

    updated dockerfile for app

---
+++ webapp1/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express ();
+
+app.get('/', (req, res) => {
+    res.send('HI there');
+});
+
+
+app.listen(8080, () =>{
+    console.log('listening on port 8080');
+
+});
